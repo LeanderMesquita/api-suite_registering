0fee558dfcd5209e150551d3a5c5bdf7f7d0443e
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-05-19 23:10:04 -0300

    subject class and subclasses

--- src/tasks/subjects.py
+++ src/tasks/subjects.py
@@ -101,3 +101,45 @@ class Lawyer(Subject):
             click_and_fill('anular_contraparte')
             click_and_fill('anular_reu')
             raise DataFillingError(f'Erro no cadastro de advogado')
+        
+class RelatedProfessionals(Subject):
+    def __init__(self, row):
+        super().__init__(row)
+
+    def select_office(self, office):
+        office_map = {
+            'TAUNAY': 'select_taunay',
+            'CML ADVOGADOS': 'select_cml',
+            'GONDIM': 'select_gondim',
+            'URBANO': 'select_urbano',
+            'VILLEMOR': 'select_villemor_civel',
+            'CLETO GOMES': 'select_cleto_gomes_civel'
+        }
+
+        office = office_map.get(office, None)
+
+        if office:
+            click_and_fill(office)
+        else:
+            print("No office defined for this condition.")
+
+    def execute(self):
+        try:
+            click_and_fill('selecionar_profissionais')
+            click_and_fill('selecionar_profissional_responsavel', self.row['RESPONSAVEL'])#ex: 'ENEIDA ZURU' 
+            click_and_fill('acrescentar_externo', delay_after=5) 
+            click_and_fill('vizualizar_assinados')
+            click_and_fill('nominativo', self.row['ESCRITORIO'])#ex: nome do escritorio
+            self.select_office(office=self.row['ESCRITORIO'])
+            click_and_fill('ok_escritorio_encarregado')
+            click_and_fill('ok_detalhe_encarregado')
+            click_and_fill('aceitar_valor_incumbencia', delay_after=3)
+            click_and_fill('finalizar_profissionais_processo')
+        except:
+            click_and_fill('anular_detalhe_encarregado')
+            click_and_fill('anular_profissional_encarregado')
+            click_and_fill('anular_profissionais')
+            click_and_fill('anular_dados_iniciais')
+            raise DataFillingError(f'Nao foi possivel preencher os profissionais')
+    
+    
