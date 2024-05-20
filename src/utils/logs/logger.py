7c32733251fd746f2d62fde373acf0d879d671f2
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-05-20 00:10:45 -0300

    logger initial structure

---
+++ src/utils/logs/logger.py
@@ -0,0 +1,6 @@
+from loguru import logger
+import os
+
+# Configurar o arquivo de log
+log_file = os.path.join('logs', 'automation.log')
+logger.add(log_file, rotation="1 MB", retention="10 days", level="INFO")
