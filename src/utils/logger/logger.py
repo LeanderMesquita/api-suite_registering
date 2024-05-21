738f060543b5f2263e6d799d0dfefe766bc922d6
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-05-21 18:21:28 -0300

    adjusting logger

--- src/utils/logger/logger.py
+++ src/utils/logger/logger.py
@@ -2,5 +2,5 @@ from loguru import logger
 import os
 
 # Configurar o arquivo de log
-log_file = os.path.join('logs', 'automation.log')
+log_file = os.path.join('api', 'logs', 'automation.log')
 logger.add(log_file, rotation="1 MB", retention="10 days", level="INFO")
