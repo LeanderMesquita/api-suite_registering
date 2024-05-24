bba422106a51aaa1fc5eba3aeedb72eef205747f
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-05-24 14:42:47 -0300

    att: read_dataframe function

--- src/utils/functions/read_dataframe.py
+++ src/utils/functions/read_dataframe.py
@@ -1,8 +1,24 @@
 import pandas as pd
 
+
+def format_numeric_value(value):
+    try:
+        float_value = float(value)
+        if float_value.is_integer():
+            return str(int(float_value))  
+        else:
+            return str(float_value).replace('.', ',')
+    except ValueError:
+        return value  
+
 def read_dataframe(path: str):
     try:
-        df = pd.read_excel(path)
+        df = pd.read_excel(path, dtype=str) 
+        
+        for col in df.columns:
+            if df[col].str.replace('.', '', 1).str.isdigit().any():
+                df[col] = df[col].apply(format_numeric_value)
+        
         return df
     except FileNotFoundError:
         print("Arquivo não encontrado.")
