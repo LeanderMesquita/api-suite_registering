cca5ce1105fa05e35c3f2c67c780b57c9217f4dc
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-06-03 13:24:16 -0300

    feat: term schedule logger

--- src/tasks/schedule.py
+++ src/tasks/schedule.py
@@ -18,46 +18,72 @@ class TermSchedule(Schedule):
         super().__init__(row)
  
     def execute(self):
+        log.info('Starting the TermSchedule registering')
         try:
+            log.debug('Openning schedule')
             click_and_fill('abrir_agenda', command='rightClick')
+            log.debug('Selecting new schedule')
             click_and_fill('nova_agenda')
+            log.debug('Selecting the term schedule type')
             click_and_fill('novo_prazo', delay_after=6)
             robot_response = decapitalize_letters(self.row['RECEBIDO POR ROBO'])
 
+            log.info('Registering the distribuition schedule')
             distribuition_date = format_date(self.row['DATA DISTRIBUICAO'])
             pyperclip.copy(distribuition_date)
+            log.debug('Selecting the date field')
             click_and_fill('selecionar_data', command='rightClick')
+            log.debug('Pasting the distribuition date')
             click_and_fill('colar_data')
+            log.debug('Filling the distribuition description')
             click_and_fill('alterar_descricao', value='DATA DE DISTRIBUICAO')
+            log.debug('Selecting another note')
             click_and_fill('selecionar_outra_nota')
+            log.debug('Accepting the schedule note')
             click_and_fill('aceitar_outra_nota', delay_after=5)
             click_and_fill('ok_data_cumprimento')
             
             if(robot_response != 'sim'):
-
+                log.info('Registering the quote schedule')
                 quote_date = format_date(self.row['DATA CITACAO'])
                 pyperclip.copy(quote_date)
+                log.debug('Selecting the data field')
                 click_and_fill('selecionar_data', command='rightClick')
+                log.debug('Pasting the quote date')
                 click_and_fill('colar_data')
+                log.debug('Inserting the quote time')
                 click_and_fill('inserir_horario', str(self.row['HORARIO CITACAO']))
+                log.debug('Filling the quote description')
                 click_and_fill('alterar_descricao', value='DATA DE CITACAO')
+                log.debug('Selecting another note')
                 click_and_fill('selecionar_outra_nota')
+                log.debug('Accepting the schedule note')
                 click_and_fill('aceitar_outra_nota', delay_after=5)
                 click_and_fill('ok_data_cumprimento')
 
+            log.info('Registering the receipt schedule')
             receipt_date = format_date(self.row['DATA RECEBIMENTO'])
             pyperclip.copy(receipt_date)
+            log.debug('Selecting the data field')
             click_and_fill('selecionar_data', command='rightClick')
+            log.debug('Pasting the receipt date')
             click_and_fill('colar_data')
+            log.debug('Inserting the receipt time')
             click_and_fill('inserir_horario', str(self.row['HORARIO RECEBIMENTO']))
             receipt_description = 'DATA DE RECEBIMENTO NO BACKOFFICE - VIA TRATAMENTO CONSOLIDADO CAPTURAS' if (robot_response == 'sim') else 'DATA DE RECEBIMENTO NO BACKOFFICE'
+            log.debug('Filling the receipt description')
             click_and_fill('alterar_descricao', value=receipt_description)
+            log.debug('Saving Schedule')
             click_and_fill('ok_agenda')
+            log.debug('Accepting the schedule note')
             click_and_fill('aceitar_outra_nota', delay_after=5)
             click_and_fill('ok_data_cumprimento', delay_after=20)
+
+            log.success('TermSchedule registered successfully!')
             
         except Exception as e:
-            raise DataFillingError(f'Error in TermSchedule registring: {e}')
+            log.error('Error during term schedule registering...')
+            raise DataFillingError(f'Error in term schedule registring: {e}')
         
 
 
