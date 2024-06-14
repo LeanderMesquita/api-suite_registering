9fe18b65cd6035f7266118644a8fc1177f7c8a43
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-06-14 16:30:12 -0300

    REFACTOR: schedule adjusting code and creating images validations

--- src/tasks/schedule.py
+++ src/tasks/schedule.py
@@ -19,64 +19,48 @@ class TermSchedule(Schedule):
     def execute(self):
         log.info('Starting the TermSchedule registering')
         try:
-            log.debug('Openning schedule')
+            self.screen.validate_image('schedule_screen_validator')
             click_and_fill('abrir_agenda', command='rightClick')
-            log.debug('Selecting new schedule')
             click_and_fill('nova_agenda')
-            log.debug('Selecting the term schedule type')
             click_and_fill('novo_prazo', delay_after=6)
             robot_response = self.row['RECEBIDO POR ROBO'].lower()
-
             log.info('Registering the distribuition schedule')
             distribuition_date = format_date(self.row['DATA DISTRIBUICAO'])
             pyperclip.copy(distribuition_date)
-            log.debug('Selecting the date field')
             click_and_fill('selecionar_data', command='rightClick')
-            log.debug('Pasting the distribuition date')
             click_and_fill('colar_data')
-            log.debug('Filling the distribuition description')
             click_and_fill('alterar_descricao', value='DATA DE DISTRIBUICAO')
-            log.debug('Selecting another note')
             click_and_fill('selecionar_outra_nota')
-            log.debug('Accepting the schedule note')
-            click_and_fill('aceitar_outra_nota', delay_after=5)
-            click_and_fill('ok_data_cumprimento')
+            click_and_fill('aceitar_outra_nota')
+            if(self.screen.check_if_exist_image('fullfillment_validator', tryes=5)):
+                click_and_fill('ok_data_cumprimento')
             
             if(robot_response != 'sim'):
+
                 log.info('Registering the quote schedule')
                 quote_date = format_date(self.row['DATA CITACAO'])
                 pyperclip.copy(quote_date)
-                log.debug('Selecting the date field')
                 click_and_fill('selecionar_data', command='rightClick')
-                log.debug('Pasting the quote date')
                 click_and_fill('colar_data')
-                log.debug('Inserting the quote time')
                 click_and_fill('inserir_horario', str(self.row['HORARIO CITACAO']))
-                log.debug('Filling the quote description')
                 click_and_fill('alterar_descricao', value='DATA DE CITACAO')
-                log.debug('Selecting another note')
                 click_and_fill('selecionar_outra_nota')
-                log.debug('Accepting the schedule note')
-                click_and_fill('aceitar_outra_nota', delay_after=5)
-                click_and_fill('ok_data_cumprimento')
+                click_and_fill('aceitar_outra_nota')
+                if(self.screen.check_if_exist_image('fullfillment_validator', tryes=5)):
+                    click_and_fill('ok_data_cumprimento')
 
             log.info('Registering the receipt schedule')
             receipt_date = format_date(self.row['DATA RECEBIMENTO'])
             pyperclip.copy(receipt_date)
-            log.debug('Selecting the date field')
             click_and_fill('selecionar_data', command='rightClick')
-            log.debug('Pasting the receipt date')
             click_and_fill('colar_data')
-            log.debug('Inserting the receipt time')
             click_and_fill('inserir_horario', str(self.row['HORARIO RECEBIMENTO']))
             receipt_description = 'DATA DE RECEBIMENTO NO BACKOFFICE - VIA TRATAMENTO CONSOLIDADO CAPTURAS' if (robot_response == 'sim') else 'DATA DE RECEBIMENTO NO BACKOFFICE'
-            log.debug('Filling the receipt description')
             click_and_fill('alterar_descricao', value=receipt_description)
-            log.debug('Saving Schedule')
             click_and_fill('ok_agenda')
-            log.debug('Accepting the schedule note')
-            click_and_fill('aceitar_outra_nota', delay_after=5)
-            click_and_fill('ok_data_cumprimento', delay_after=20)
+            click_and_fill('aceitar_outra_nota')
+            if(self.screen.check_if_exist_image('fullfillment_validator', tryes=5)):
+                click_and_fill('ok_data_cumprimento')
 
             log.success('TermSchedule registered successfully!')
             
@@ -94,28 +78,22 @@ class HearingSchedule(Schedule):
     def execute(self):
         log.info('Starting the Hearing Schedule registering')
         try:
-            log.debug('Openning schedule')
+            
             click_and_fill('abrir_agenda', command='rightClick')
-            log.debug('Selecting new schedule')
             click_and_fill('nova_agenda')
-            log.debug('Selecting the hearing schedule type')
             click_and_fill('nova_audiencia', delay_after=6)
             hearing_date = format_date(self.row['DATA AUDIENCIA'])
             pyperclip.copy(hearing_date)
-            log.debug('Selecting the date field')
             click_and_fill('selecionar_data', command='rightClick')
-            log.debug('Pasting the hearing date')
             click_and_fill('colar_data')
-            log.debug('Inserting the hearing time')
             click_and_fill('inserir_horario', str(self.row['HORARIO AUDIENCIA']))
-            log.debug('Filling the hearing description')
             click_and_fill('alterar_descricao', value='CONCILIACAO')
-            log.debug('Saving schedule')
             click_and_fill('ok_agenda')
-            log.debug('Accepting the schedule note')
-            click_and_fill('aceitar_outra_nota', delay_after=5)
-            click_and_fill('ok_data_cumprimento', delay_after=10)
+            click_and_fill('aceitar_outra_nota')
+            if(self.screen.check_if_exist_image('fullfillment_validator', tryes=5)):
+                click_and_fill('ok_data_cumprimento')
             log.success('Hearing schedule registered successfully!')
+
         except Exception as e:
             log.error(f'Error during the hearing schedule registering: {e}')
             raise DataFillingError(f'Error in Hearing schedule registering: {e}')
@@ -126,26 +104,21 @@ class TutelageSchedule(Schedule):
 
     def execute(self):
         try:
-            log.debug('Openning schedule')
+            
             click_and_fill('abrir_agenda', command='rightClick')
-            log.debug('Selecting new schedule')
             click_and_fill('nova_agenda')
-            log.debug('Selecting the decision schedule type')
             click_and_fill('nova_decisao', delay_after=6)
             tutelage_date = format_date(self.row['DATA TUTELA'])
             pyperclip.copy(tutelage_date)
-            log.debug('Selecting the date field')
             click_and_fill('selecionar_data_decisao', command='rightClick')
-            log.debug('Pasting the tutelage date')
             click_and_fill('colar_data_decisao')
-            log.debug('Filling the tutelage description')
             click_and_fill('alterar_descricao', value='TUTELA - '+self.row['DESCRICAO TUTELA'])
-            log.debug('Saving schedule')
             click_and_fill('ok_agenda')
-            log.debug('Accepting the schedule note')
-            click_and_fill('aceitar_outra_nota', delay_after=5)
-            click_and_fill('ok_data_cumprimento', delay_after=10)
+            click_and_fill('aceitar_outra_nota')
+            if(self.screen.check_if_exist_image('fullfillment_validator', tryes=5)):
+                click_and_fill('ok_data_cumprimento')
             log.success('Tutelage schedule registered successfully!')
+
         except Exception as e:
             log.error(f'Error during tutelage schedule registering: {e}')
             raise DataFillingError(f'Error during TutelageSchedule registering: {e}')
