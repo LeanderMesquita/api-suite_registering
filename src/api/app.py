6aa54b15be5334ff5421c188a9af1fb184b7a43f
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-06-18 14:25:50 -0300

    REFACTOR: changes on app flux

--- src/api/app.py
+++ src/api/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from factory import TaskFactory
 from utils.functions.error_report import error_report
 from utils.functions.click_and_fill import click_and_fill
+from utils.functions.read_dataframe import read_dataframe
 from utils.functions.successfully_report import successfully_report
 from utils.logger.logger import log
 
@@ -35,22 +36,21 @@ def upload_file():
 def start_automation(file_path):
     try:
         log.info('Starting automation')
-        df = pd.read_excel(file_path)
+        df = read_dataframe(file_path)
         if not (df.empty): log.success('Dataframe create sucessfully!')
         for index, row in df.iterrows():
             try:
                 log.info(f'Registering process: ({row['NUMERO DO PROCESSO']}).')
-                situational_init_key = 'novo_processo' if index == 0 else 'processo_seguinte'
-                click_and_fill(situational_init_key)
-                task = TaskFactory.create_task('author', row)
+                task = TaskFactory.create_task('register_process', row)
                 task.execute()
                 log.success(f'Process ({row['NUMERO DO PROCESSO']}) was registered with success!')
+                if index >= len(df) - 1: log.warning(f'All process are readed, please verify the reports. Iterate qtn{len(df)}')
                 successfully_report(row['NUMERO DO PROCESSO'], row['AUTOR'])
             except Exception as e:
                 log.error(f'The process: ({row['NUMERO DO PROCESSO']}) was not registered. {e}')
                 log.info('Moving on to the next process...')
                 error_report(row['NUMERO DO PROCESSO'], row['AUTOR'], error=e)
-                continue
+                
     except Exception as e:
         log.critical(f"An critical error occurred!: {e}")
          
