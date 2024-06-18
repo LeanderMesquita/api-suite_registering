9c0d89b8cf1f270511a9ac8a13b503a96c6443ff
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-06-18 18:22:00 -0300

    FIX: adjusting dataError text

--- api/src/tasks/register_process.py
+++ api/src/tasks/register_process.py
@@ -46,4 +46,4 @@ class RegisterProcess(BaseTask):
                 self.tutelage.execute()
             click_and_fill('encerrar_processo')
         except Exception as e:
-            raise DataFillingError(f'An error ocurred during full registration {e}')
+            raise DataFillingError(f'An error ocurred! (Register Process): {e}')
