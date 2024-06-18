295f6a43282badcd7846cd70bf39516e6c4cad0a
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-06-18 17:12:26 -0300

    DELETE: older structure

--- src/tasks/base_task.py
+++
@@ -1,6 +0,0 @@
-class BaseTask:
-    def __init__(self, row):
-        self.row = row
-
-    def execute(self):
-        raise NotImplementedError('Subclasses must implements the execute method.')
--- src/utils/logger/logger.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-import os
-
-# Configurar o arquivo de log
-log_file = os.path.join('api', 'logs', 'automation.log')
-logger.add(log_file, rotation="1 MB", retention="10 days", level="INFO", format="{time} - {level} - {message}")
-
-log = logger
